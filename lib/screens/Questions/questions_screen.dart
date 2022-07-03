9c9342478a1338a9143fefaeb9f3817027f918c4
mirsaid ismailov <mirsaid.ismailov@alchemy.com.tr>
2022-07-03 17:02:45 +0300

    questions edit

--- lib/screens/Questions/questions_screen.dart
+++ lib/screens/Questions/questions_screen.dart
@@ -1,3 +1,4 @@
+import 'package:ehliyetim/theme/colors.dart';
 import 'package:ehliyetim/widgets/year_section.dart';
 import 'package:flutter/material.dart';
 import '../../widgets/custom_app_bar.dart';
@@ -16,6 +17,7 @@ class QuestionsScreen extends StatelessWidget {
             Expanded(
               child: SingleChildScrollView(
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     SizedBox(height: 20),
                     YearSection(year: 2022, availableMonthsCount: 7),
@@ -23,7 +25,35 @@ class QuestionsScreen extends StatelessWidget {
                     YearSection(year: 2021, availableMonthsCount: 12),
                     SizedBox(height: 20),
                     YearSection(year: 2020, availableMonthsCount: 12),
-                    SizedBox(height: 500),
+                    SizedBox(height: 20),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10.0),
+                      child: Text(
+                        '2014-2018',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 14, vertical: 20),
+                      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+                      decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(5)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Flexible(
+                            child: Text(
+                              '24 Ocak 2014 Ehliyet Sınavı Soruları',
+                              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                          Icon(Icons.arrow_forward_ios, size: 16, color: MyColors.purpleLight),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
