297221327c64082989cf9c55121410810fcb53c7
Serdar <serdar.yildiz@alchemy.com.tr>
2022-07-03 18:52:57 +0300

    working with theme

--- lib/theme/theme_data.dart
+++ lib/theme/theme_data.dart
@@ -5,3 +5,44 @@ ThemeData themeData = ThemeData(
   scaffoldBackgroundColor: MyColors.greyLightest,
   fontFamily: 'ReadexPro',
 );
+
+ThemeData darkTheme = ThemeData.dark().copyWith(
+  primaryColor: Color(0xff1f655d),
+);
+
+ThemeData lightTheme = ThemeData.light().copyWith(
+  primaryColor: Color(0xfff5f5f5),
+);
+enum ThemeType { Light, Dark, System }
+
+class ThemeModel extends ChangeNotifier {
+  ThemeData currentTheme = lightTheme;
+  ThemeType _themeType = ThemeType.Dark;
+
+  toggleTheme() {
+    if (_themeType == ThemeType.Dark) {
+      currentTheme = lightTheme;
+      _themeType = ThemeType.Light;
+      return notifyListeners();
+    }
+
+    if (_themeType == ThemeType.Light) {
+      currentTheme = darkTheme;
+      _themeType = ThemeType.Dark;
+      return notifyListeners();
+    }
+  }
+}
+
+class ThemeNotifier with ChangeNotifier {
+  ThemeData _themeData;
+
+  ThemeNotifier(this._themeData);
+
+  getTheme() => _themeData;
+
+  setTheme(ThemeData themeData) async {
+    _themeData = themeData;
+    notifyListeners();
+  }
+}
